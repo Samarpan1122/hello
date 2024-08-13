b35632ed501aca82a345c3130e0b678f003943c1
Samarpan1122 <samarpanmohanty@gmail.com>
2024-08-13 14:05:36 -0500

    add a.out in git ignore and hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Samarpan Mohanty
+ *         smohanty13@unl.edu
+ * Date: 2024/08/13
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Samarpan Mohanty\n");
+
+  return 0;
+}
